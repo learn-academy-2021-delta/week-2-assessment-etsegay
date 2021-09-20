19391071e2bf361b210786dcd219a329193dc5a0
EliasEritrea <eliaszerai@gmail.com>
2021-09-20 10:45:19 -0400

    week-2-assesment completed

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,11 @@
 // --------------------1) What will this log?
 
 var cohort = "Delta 2021"
-// console.log(cohort.split(""))
+ console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:['D', 'e', 'l', 't','a', ' ', '2', '0','2', '1']
+// b) Verify and explain:An array of a string each character separtated by a comma  including the space will be
+//   printed and this is because each character including the space has an index 
 
 
 // --------------------2) What will this log?
@@ -19,28 +20,34 @@ var cohort = "Delta 2021"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+ console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:"undefined"
+// b) Verify and explain:it is verified in the console
+//the reason is  because the function has no return key word.
 
 
 // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[ 8, 10, 12, 14, 16 ]
+// b) Verify and explain:verified in the terminal and it is true
+// because map is a higher order function that applies a certain
+//task to each element of an array and returns a new array of equal size
+// of the original array
 
 
 // --------------------4) What will this log?
 
 var oddsOnly = [11, 12, 13, 14, 15].filter(value => value % 2 === 0)
-// console.log(oddsOnly)
+ console.log(oddsOnly)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[ 12, 14 ]
+// b) Verify and explain:It is verified by running the code in the 
+//terminal.This is true because filter applies a certaing task on the 
+//the elements of an array that satsify a certain given condition
 
 
 // --------------------5) What will this log?
@@ -53,7 +60,9 @@ class Learn {
   }
 }
 var learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:{ student: 'George', cohort: 'Delta', year: 2021 }
+// b) Verify and explain:it is true,because when an object is created the constructor function assigne the 
+//the given value to the this keyword.Before the object created the this key word creates an 
+//empty object.
